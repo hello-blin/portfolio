bc38b24036015edad62f4d14562f4c955ae71da2
Blin Kukaj <kblin164@gmail.com>
2022-06-10 13:18:23 +0200

    Finished the second section called About, this sticks to only desktop screens. Used flex for positioning as its my main style of positioning

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,17 +1,24 @@
 import React from 'react'
 import './About.scss'
+import { images } from '../../constants/constants'
 function About() {
   return (
     <div className='about-container'>
-      <div className='about-container__headline'>
-        <h1 className='about-headline'>
-          No Sugar on my coffee
-        </h1>
+      <div className="about-first__column">
+        <div className='about-container__headline'>
+          <h1 className='about-headline'>
+            No Sugar on my coffee
+          </h1>
+        </div>
+        <div className="about-container__description">
+          <span className='about-headline__description'>
+            Hi, my name is Blin Kukaj and I come from Kosovo. Programming started as more of a hobby for me, but now it's definitely a great passion.
+            As a Back End Developer, I started by making sample projects as a 14 year old. I found it sort of boring, however oportunities burst out and I began working on out-source projects for a very cheap price. After spending more and more time coding, I started to like the problem-solving aspect of it and quickly became addicted to the high of figuring out a difficult problem.
+          </span>
+        </div>
       </div>
-      <div className="about-container__description">
-        <span className='about-headline__description'>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nisi laudantium expedita tenetur a similique quam, laboriosam maiores voluptatem vero, esse autem optio asperiores sequi eligendi nesciunt quo explicabo sit excepturi.
-        </span>
+      <div className="about-second__column">
+        <img src={images.hey} alt="" />
       </div>
     </div>
   )
--- src/constants/images.js
+++ src/constants/images.js
@@ -2,11 +2,12 @@ import nodejs from '../assets/nodejs.svg'
 import reactjs from '../assets/reactjs.svg'
 import mongo from '../assets/mongo.svg'
 import blini from '../assets/blini.webp'
-
+import hey from '../assets/hey.png'
 
 export default {
     nodejs,
     reactjs,
     mongo,
-    blini
+    blini,
+    hey
 }
