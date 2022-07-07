847de62ed5776947e464f560833de1fd96b21876
Blin Kukaj <kblin164@gmail.com>
2022-07-07 12:35:20 +0200

    Hello

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -14,15 +14,14 @@ function About() {
         <div className="about-container__description">
           <span className='about-headline__description'>
             Hi, my name is Blin Kukaj and I come from Kosovo. Programming started as more of a hobby for me, but now it's definitely a great passion.
-            As a Web Developer, I started by making sample projects as a 14 year old. I found it sort of boring, however oportunities burst out and I began working on out-source projects for a very cheap price. After spending more and more time coding, I started to like the problem-solving aspect of it and quickly became addicted to the high of figuring out a difficult problem.
+            As a Web Developer, I startedd by making sample projects as a 14 year old. I found it sort of boring, however oportunities burst out and I began working on out-source projects for a very cheap price. After spending more and more time coding, I started to like the problem-solving aspect of it and quickly became addicted to the high of figuring out a difficult problem.
           </span>
         </div>
       </div>
       <div className="about-second__column">
-        <div className="inner-second__div" aniamte={{ x: 100 }} transition={{ ease: "easeOut", duration: 2 }}>
+        <div className="inner-second__div">
           <img src={images.hey} alt="" />
         </div>
-
       </div>
     </div>
   )
